06db7d2b8817bf1b53aaacbe00e6f487afbe7a30
robiul@hasan <robihasan093@gmail.com>
2025-07-10 12:06:31 +0600

    implement userList, userName popup, polyLine feature

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
-const express = require('express')
-const app = express()
-const http = require('http')
-const path = require('path')
-const socketio = require('socket.io')
-const server = http.createServer(app)
-const io = socketio(server)
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection", function(socket){
-    socket.on("send-location", function(data) {
-        io.emit("receive-location", {id: socket.id, ...data})
-    })
-    socket.on("diconnect", function() {
-        io.emit("user-disconnected", {id: socket.id});
-    })
-})
+const users = {};
+function broadcastUserList() {
+  io.emit("update-users", Object.values(users));
+}
 
-app.get('/', function (req, res){
-    res.render('index')
+io.on("connection", function (socket) {
+  console.log(`user connected: ${socket.id}`);
+
+  socket.on("send-location", function (data) {
+    users[socket.id] = data.userName || "Unknown";
+    io.emit("receive-location", {
+      id: socket.id,
+      userName: users[socket.id],
+      latitude: data.latitude,
+      longitude: data.longitude,
+    });
+
+    broadcastUserList();
+
+  });
+  socket.on("disconnect", function () {
+    delete users[socket.id];
+    io.emit("user-disconnected", socket.id);
+    broadcastUserList();
+  });
+});
+
+app.get("/", function (req, res) {
+  res.render("index");
 });
 
 server.listen(5000, () => {
-    console.log("Server running on http://localhost:5000");
+  console.log("Server running on http://localhost:5000");
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,40 +1,83 @@
 const socket = io();
 
-if(navigator.geolocation){
-    navigator.geolocation.watchPosition((position) => {
-        const {latitude, longitude} = position.coords;
-        console.log("Latitude =>", latitude, "Longitude =>", longitude)
-        socket.emit("send-location", {latitude, longitude})
-    }, (error) => {
-        console.log("Error", error)
-    }, {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-    })
+let userName = prompt("Enter your name:");
+if (!userName) userName = "Anonymous";
+
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      console.log("Latitude =>", latitude, "Longitude =>", longitude);
+      socket.emit("send-location", { latitude, longitude, userName });
+    },
+    (error) => {
+      console.log("Error", error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    }
+  );
 }
 
+const map = L.map("map").setView([0, 0], 2);
 
-const map = L.map("map").setView([0, 0], 17);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",).addTo(map)
-
-const markers = {}
+const markers = {};
+const trails = {};
 
 socket.on("receive-location", (data) => {
-    const {id, latitude, longitude} = data;
-    map.setView([latitude, longitude])
-    if(markers[id]){
-        markers[id].setLatLng([latitude, longitude])
-    }
-    else{
-        markers[id] = L.marker([latitude, longitude]).addTo(map);
-    }
-})
+  const { id, latitude, longitude, userName } = data;
+  map.setView([latitude, longitude]);
+  if (!markers[id]) {
+    markers[id] = L.marker([latitude, longitude])
+      .addTo(map)
+      .bindPopup(id === socket.id ? userName : `${socket.id}`)
+      .openPopup();
+  } else {
+    markers[id].setLatLng([latitude, longitude]);
+  }
 
-socket.on("user-disconnected", function(id) {
-    if(markers[id]){
-        map.removeLayer(markers[id]);
-        delete markers[id]
+  if (id === socket.id) {
+    map.setView([latitude, longitude], 17);
+  }
+
+  if (!trails[id]) trails[id] = [];
+
+  trails[id].push([latitude, longitude]);
+
+  if (trails[id].length > 1) {
+    if (trails[id].polyline) {
+      trails[id].polyline.setLatLngs(trails[id]);
+    } else {
+      trails[id].polyline = L.polyline(trails[id], { color: "blue" }).addTo(
+        map
+      );
     }
-})
+  }
+});
+
+socket.on("update-users", (userList) => {
+  const list = document.getElementById("userList");
+  list.innerHTML = "";
+  userList.forEach((name) => {
+    const li = document.createElement("li");
+    li.textContent = name;
+    list.appendChild(li);
+  });
+});
+
+socket.on("user-disconnected", function (id) {
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+
+  if (trails[id] && trails[id].polyline) {
+    map.removeLayer(trails[id].polyline);
+  }
+
+  delete trails[id];
+});
